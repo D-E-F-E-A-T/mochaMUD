8da9121150af541804006194ca5573ab15e72752
_c0da_ <duvictor514@gmail.com>
2017-12-08 16:11:11 -0800

    Made install script POSIX compliant.

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
-if [[ $EUID -ne 0 ]]; then
-   echo "Please run the coffee{MUD} installer as root." 
-   exit 1
+ROOTUID="0"
+
+if [ "$(id -u)" -ne "$ROOTUID" ] ; then
+    echo "This script must be executed with root privileges."
+    exit 1
 fi
-echo "Would you like to install coffee{MUD}?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) break;;
-        No ) exit;;
-    esac
-done
-echo "Installing coffee{MUD}..."
-mkdir /opt/coffeeMUD-client
-cp Precompiled/coffeeMUD.jar /opt/coffeeMUD-client
-echo "Copied coffee{MUD} executable to /opt/coffeeMUD-client"
-echo "Creating command runnable as 'coffeemud'"
-touch /usr/bin/coffeemud
-echo "java -jar /opt/coffeeMUD-client/coffeeMUD.jar" > /usr/bin/coffeemud
-chmod 755 /usr/bin/coffeemud
-echo "Installation complete"
+
+echo -n "Would you like to install coffee{MUD}? (Y/N)"
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+    echo "Installing coffee{MUD}..."
+	mkdir /opt/coffeeMUD-client
+	cp Precompiled/coffeeMUD.jar /opt/coffeeMUD-client
+	echo "Copied coffee{MUD} executable to /opt/coffeeMUD-client"
+	echo "Creating command runnable as 'coffeemud'"
+	touch /usr/bin/coffeemud
+	echo "java -jar /opt/coffeeMUD-client/coffeeMUD.jar" > /usr/bin/coffeemud
+	chmod 755 /usr/bin/coffeemud
+	echo "Installation complete"
+    
+else
+    echo "Cancelled installation."
+fi
